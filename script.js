a0fa560122f4d5e7a442c1f6a9655d664e275d2d
Kunimitsu <101797828+HanifNahan@users.noreply.github.com>
2022-06-25 17:32:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let weather = {
+    "apiKey": "2d56a9e31a7b674593c4331962e36657",
+    fetchWeather: function(city) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`).then((Response) => Response.json()).then((data) => this.displayWeather(data))
+    },
+    displayWeather: function(data) {
+        const {name} = data;
+        const {icon,description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = `Weather in ${name}`;
+        document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = `${temp}°C`;
+        document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`;
+        document.querySelector(".wind").innerText = `Wind speed: ${speed}km/h`;
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+}
+
+document.querySelector(".search button").addEventListener("click", function() {
+    weather.search();
+})
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+})
+
+weather.fetchWeather("perlis");
